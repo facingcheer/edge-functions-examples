59fcc5ad17031d4514d08f508f36f21906a8e3d6
季金龙 <jijinlong@bytedance.com>
2022-10-14 17:23:29 +0800

    feat: add url

--- netlify/edge-functions/jms-decode.ts
+++ netlify/edge-functions/jms-decode.ts
@@ -1,7 +1,9 @@
 import { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
-  const url = new URL(request.url);
+  const url = new URL(
+    "https://jmssub.net/members/getsub.php?service=442420&id=e6ecc0b5-2a4d-43ba-8ee8-383d67d7ca41",
+  );
 
   // Look for the query parameter, and return if we don't find it
   if (url.searchParams.get("method") !== "transform") {
