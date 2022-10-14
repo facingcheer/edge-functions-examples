43652d673fbf3b36f6186819be1b65184948efe1
季金龙 <jijinlong@bytedance.com>
2022-10-14 17:40:43 +0800

    f

--- netlify/edge-functions/jms-decode.ts
+++ netlify/edge-functions/jms-decode.ts
@@ -1,34 +1,12 @@
-// import { Context } from "https://edge.netlify.com";
-
-// export default async (request: Request, context: Context) => {
-//   const url = new URL(
-//     "https://jmssub.net/members/getsub.php?service=442420&id=e6ecc0b5-2a4d-43ba-8ee8-383d67d7ca41",
-//   );
-
-//   // Look for the query parameter, and return if we don't find it
-//   if (url.searchParams.get("method") !== "jms-decode") {
-//     return;
-//   }
-
-//   context.log(`Transforming the response from this ${url}`);
-
-//   const response = await context.next();
-
-//   const text = await response.text();
-//   return new Response(atob(text), response);
-// };
-
 import { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
   const joke = await fetch(
     "https://jmssub.net/members/getsub.php?service=442420&id=e6ecc0b5-2a4d-43ba-8ee8-383d67d7ca41",
     {
-      headers: {
-        Accept: "text/html",
-      },
+      headers: {},
     },
   );
   const textData = await joke.text();
-  return context.text(atob(textData));
+  return new Response(atob(textData));
 };
